ccdd5254edcd8fafc7a5a5c17f584fd447162d8c
Jamie Findlay <jy.findlay@gmail.com>
2026-07-30 00:23:03 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Home Manager-based setup for non-NixOS machines";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Spicetify: for prettifying Spotify
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, spicetify-nix }: {
+    homeConfigurations."jamief" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit spicetify-nix };
+      modules = [ ./home.nix ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,138 @@
+{ config, pkgs, spicetify-nix, ... }:
+
+let
+  spicePkgs = spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+in
+{
+  imports = [
+	  spicetify-nix.homeManagerModules.default
+  ];
+  
+  ## BASIC SETTINGS
+  home.username = "jamief";
+  home.homeDirectory = "/home/jamief";
+  home.stateVersion="26.05"
+
+  ## SYMLINKING DOTFILES
+  home.file = {
+    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.zshrc;
+    ".zshenv".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.zshenv;
+    ".zsh".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.zsh;
+    ".vimrc".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.vimrc;
+    ".latexmkrc".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.latexmkrc;
+    ".dir_colors".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.dir_colors;
+    ".config/alacritty".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.config/alacritty;
+    ".config/cosmic".source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.config/cosmic; # Config for COSMIC DE – comment out/delete if using another DE
+    ".config/cosmic" = {
+      source = config.lib.file.mkOutOfStoreSymlink /home/jamief/.dotfiles/.config/cosmic;
+      force = true;
+    };
+  };
+
+  # Setting some default applications
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      # Browser  
+      "text/html" = "brave-browser.desktop"; 
+      "x-scheme-handler/http" = "brave-browser.desktop";
+      "x-scheme-handler/https" = "brave-browser.desktop";
+      "x-scheme-handler/chrome" = "brave-browser.desktop";
+      "application/x-extension-htm" = "brave-browser.desktop";
+      "application/x-extension-html" = "brave-browser.desktop";
+      "application/x-extension-shtml" = "brave-browser.desktop";
+      "application/xhtml+xml" = "brave-browser.desktop";
+      "application/x-extension-xhtml" = "brave-browser.desktop";
+      "application/x-extension-xht" = "brave-browser.desktop";
+      "x-scheme-handler/about" = "brave-browser.desktop";
+    };
+  };
+
+  # Allow Home Manager to update the font cache
+  fonts.fontconfig.enable = true;
+  
+  ## USER PACKAGES
+  # List of packages without their own modules
+  home.packages = with pkgs; [
+    vim
+    alacritty
+    git
+    wl-clipboard
+    tree
+    tldr
+    brave
+    discord
+    # texliveFull
+    ## Python
+    python3
+    python3Packages.isort
+    python3Packages.pytest
+    pipenv
+    ## Haskell
+    haskellPackages.ghc
+    haskellPackages.cabal-install
+    haskellPackages.hoogle
+    haskell-language-server
+    ## Doom emacs requirements
+    fd
+    ripgrep
+    pandoc                     # for markdown compiling
+    shellcheck
+    grip
+    nixfmt                     # formatting for nix
+    zig                        # for building ghostel
+    ## Fonts
+    nerd-fonts.jetbrains-mono  # JetBrains Mono
+    nerd-fonts.symbols-only
+    fira                       # Fira Sans
+    symbola
+    source-sans
+  ];
+
+  # Extensions for Chromium browsers like Brave (IDs from Chrome Web Store URL)
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "fdjamakpfbbddfjaooikfcpapjohcfmg" # Dashlane
+      "aapbdbdomjkkjkaonfhkkikfgjllcleb" # Google Translate
+    ];
+  };
+  
+  # Git settings
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Jamie Findlay";
+      user.email = "jy.findlay@gmail.com";
+      init.defaultBranch = "main";
+      pull.rebase = false;
+    };
+  };
+
+  # Emacs
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs-pgtk;
+    defaultEditor = true;
+  };
+
+  # Firefox
+  programs.firefox.enable = true;
+
+  # Install Dropbox and run the daemon
+  services.dropbox.enable = true;
+
+  # Install and prettify Spotify
+  programs.spicetify = {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      shuffle
+      hidePodcasts
+      popupLyrics
+      betterGenres
+    ];
+    theme = spicePkgs.themes.sleek;
+    colorScheme = "Nord";
+  };
+
+}
